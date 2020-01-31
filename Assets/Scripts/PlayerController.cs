2919e8424d3feb3ef5292d09cb919824ec5b2727
Ricardo de la rosa vivas <ricardodelarosavivas@gmail.com>
2020-01-31 23:00:32 +0100

    Añadido tag de stairs ---- aladdio trigger al player

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,10 +6,12 @@ public class PlayerController : MonoBehaviour
 {
     public GameSettingsSO gameSettings;
     public Rigidbody2D m_rigidBody;
+    bool bIsInsideStairCollider = false;
 
     void Update()
     {
         float velocidadEjeX = 0 ; // -1 izq, 1 der, 0 quieto
+        float velocidadEjeY = 0 ; // 
         if (Input.GetKey(KeyCode.D))
         {
             //Velocidad positiva
@@ -20,7 +22,37 @@ public class PlayerController : MonoBehaviour
             //Velocidad negativa
             velocidadEjeX = -1;
         }
+
+        if(Input.GetKey(KeyCode.S) && bIsInsideStairCollider)
+        {
+            velocidadEjeY = -1;
+        }
         
-        m_rigidBody.velocity = new Vector2(velocidadEjeX * gameSettings.playerHorSpeed, m_rigidBody.velocity.y);
+        if(Input.GetKey(KeyCode.W) && bIsInsideStairCollider)
+        {
+            velocidadEjeY = 1;
+        }
+        
+        transform.Translate(new Vector2(Time.deltaTime * velocidadEjeX * gameSettings.playerHorSpeed,
+                            Time.deltaTime * velocidadEjeY * gameSettings.playerVerSpeed));
+        // m_rigidBody.velocity = new Vector2(m_rigidBody.velocity.x, gameSettings.playerVerSpeed * velocidadEjeY);
+        // m_rigidBody.velocity = new Vector2(velocidadEjeX * gameSettings.playerHorSpeed, m_rigidBody.velocity.y);
+    }
+
+    void OnTriggerEnter2D(Collider2D collider2D)
+    {
+        if(collider2D.CompareTag("Stairs"))
+        {
+            bIsInsideStairCollider = true;
+            Debug.Log("Enter stairs");
+        } 
+    }
+    void OnTriggerExit2D(Collider2D collider2D)
+    {
+        if(collider2D.CompareTag("Stairs")) 
+        {
+            bIsInsideStairCollider = false;
+            Debug.Log("Exit stairs");
+        }
     }
 }
