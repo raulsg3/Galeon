1db3d5ff4d70e3fe60c0485500f318ede4ff2be2
Ricardo de la rosa vivas <ricardodelarosavivas@gmail.com>
2020-01-31 21:02:48 +0100

    GameSettings creado

---
+++ Assets/Scripts/GameSettingsSO.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName ="Galeon/GameSettings", fileName ="GameSettings")]
+public class GameSettingsSO : ScriptableObject
+{
+    public float playerHorSpeed = 2f;
+    public float playerVerSpeed = 2f;
+}
