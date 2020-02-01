d802d99b422813c5e6cf8eb5757166f7a31986f6
linmdotor <lin.midott@gmail.com>
2020-02-01 12:17:31 +0100

    Updated shoot animation

---
+++ Assets/WeaponController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponController : MonoBehaviour
+{
+    public GameObject bulletPrefab;
+    public GameObject spawnPosition;
+
+    // Instaciate the bullet prefab
+    void Shoot()
+    {
+        Debug.Log("DISPARA!");
+        //Instantiate(myPrefab, spawnPosition., Quaternion.identity);
+    }
+}
