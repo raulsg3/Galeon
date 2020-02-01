a8c7ce766fa0d657aa271bd5b99a46b6fa2f20ee
raulsg3 <raulseguragaleote@gmail.com>
2020-02-01 16:11:01 +0100

    Script placeholder

---
+++ Assets/Scripts/Enemy/EnemyMoveRepairable.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyMoveRepairable : MonoBehaviour
+{
+    /*
+    private GameObject target;
+
+    public float speed = 5f;
+
+	void Start () {
+        player = GameObject.FindGameObjectWithTag(Tags.Player);
+	}
+	
+	void Update () {
+        if (Mathf.Abs(player.transform.position.x - transform.position.x) > player.GetComponent<PlayerData>().width)
+        {
+            float direction = player.transform.position.x - transform.position.x;
+
+            Vector3 directionVector = new Vector3(direction, 0, 0);
+            directionVector.Normalize();
+
+            transform.Translate(directionVector * speed * Time.deltaTime);
+        }
+	}
+    */
+}
