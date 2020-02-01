15f731900d93a100533b48a01c9804387a34a3cf
raulsg3 <raulseguragaleote@gmail.com>
2020-02-01 18:55:58 +0100

    Enemies destroying repairable objects

--- Assets/Scripts/Enemy/EnemyMoveRepairable.cs
+++ Assets/Scripts/Enemy/EnemyMoveRepairable.cs
@@ -3,25 +3,63 @@ using System.Collections;
 
 public class EnemyMoveRepairable : MonoBehaviour
 {
-    /*
-    private GameObject target;
+    private RepairableObject target = null;
+    private bool destroying = false;
 
     public float speed = 5f;
 
-	void Start () {
-        player = GameObject.FindGameObjectWithTag(Tags.Player);
+    //Daño
+    public float attackTime = 5.0f;
+    private float accAttackTime = 0.0f;
+
+    void Start () {
 	}
 	
 	void Update () {
-        if (Mathf.Abs(player.transform.position.x - transform.position.x) > player.GetComponent<PlayerData>().width)
+        if (target == null || target.IsDestoyed())
+            GetRepairableTarget();
+
+        if (!destroying && target != null && !target.IsDestoyed())
         {
-            float direction = player.transform.position.x - transform.position.x;
+            float direction = target.transform.position.x - transform.position.x;
 
             Vector3 directionVector = new Vector3(direction, 0, 0);
             directionVector.Normalize();
 
             transform.Translate(directionVector * speed * Time.deltaTime);
         }
+
+        if (destroying && target != null)
+        {
+            if (!target.IsDestoyed())
+            {
+                accAttackTime += Time.deltaTime;
+
+                if (accAttackTime >= attackTime)
+                {
+                    accAttackTime = 0.0f;
+                    target.TakeDamage();
+                }
+            }
+
+            if (target.IsDestoyed())
+            {
+                destroying = false;
+            }
+        }
 	}
-    */
+
+    //Busca el objetivo destruible más cercano
+    void GetRepairableTarget()
+    {
+        GameObject closestTarget = RepairableManager.repairableManagerInstance.GetClosestObjectInFloor(transform.position);
+        if (closestTarget != null)
+            target = closestTarget.GetComponent<RepairableObject>();
+    }
+
+    void OnTriggerEnter2D(Collider2D collider2D)
+    {
+        if (collider2D.CompareTag(Tags.RepairableObject))
+            destroying = true;
+    }
 }
