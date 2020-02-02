fb4ee3e70ff30e3a899c5fd059e6b095c99d6234
Ricardo de la rosa vivas <ricardodelarosavivas@gmail.com>
2020-02-02 12:36:54 +0100

    bocadillo enemigos

--- Assets/Scripts/Bocadillos/BocadilloController.cs
+++ Assets/Scripts/Bocadillos/BocadilloController.cs
@@ -9,4 +9,46 @@ public class BocadilloController : MonoBehaviour
    public GameObject pivotGO;
 
    public TMPro.TextMeshPro textInBocadillo;
+    public Transform parent;
+
+    private bool bEnemyHasBocadillo = false;
+    void Start()
+    {
+        pivotGO.SetActive(false);
+        if(Random.Range(0,100) < 20){
+            bEnemyHasBocadillo = true;
+            showBocadilloCooldown = Random.Range(1,5);
+        }
+    }
+    float showBocadilloCooldown =1f;
+    void Update()
+    {
+
+        if(parent.transform.localScale.x == 1)
+        {
+            transform.localScale = new Vector3(1,1,1);
+
+        }else if(parent.transform.localScale.x == -1)
+        {
+            transform.localScale = new Vector3(-1,1,1);
+        }
+        if(!bEnemyHasBocadillo) return;
+        
+        showBocadilloCooldown -= Time.deltaTime;
+
+        if(showBocadilloCooldown < 0)
+        {
+            bEnemyHasBocadillo = false;
+            textInBocadillo.text = frasesListSO.list[Random.Range(0,frasesListSO.list.Count)].frase;
+            pivotGO.SetActive(true);
+            StartCoroutine(C_HideBocadillo());
+        }
+        
+    }
+    IEnumerator C_HideBocadillo()
+    {
+        yield return new WaitForSeconds(4f);
+        pivotGO.SetActive(false);
+    }
+
 }
