8ef6f24fbc1dd6aba9046aacd8383e37b379d5bc
Ricardo de la rosa vivas <ricardodelarosavivas@gmail.com>
2020-02-02 12:07:57 +0100

    Bocadiollos

---
+++ Assets/Scripts/Bocadillos/BocadillosFrasesListSO.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Galeon/Frases/FraseLista", fileName = "FraseLista")]
+public class BocadillosFrasesListSO : ScriptableObject
+{
+    public List<BocadillosFraseSO> list;
+}
